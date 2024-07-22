dae2304119907c81589d5b71fafe855be2d2a5c6
Taisiia Galkina <galkina.tasya@mail.ru>
2024-07-22 22:44:23 +0300

    Initial Commit

---
+++ WeatherType2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherType2
+//
+//  Created by Тася Галкина on 22.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
