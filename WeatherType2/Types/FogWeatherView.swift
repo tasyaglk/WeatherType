71bd4d2c396a2390fa4381256ea96db0b182900d
Taisiia Galkina <galkina.tasya@mail.ru>
2024-07-22 23:59:04 +0300

    анимация тумана более приятная

--- WeatherType2/Types/FogWeatherView.swift
+++ WeatherType2/Types/FogWeatherView.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class FogWeatherView: UIView {
     private var fogImageViews = [UIImageView]()
-    private let fogImageCount = 3
-
+    private let fogImageCount = 5
+    private let imageSize: CGFloat = 200
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor(red: 0.68, green: 0.85, blue: 0.90, alpha: 1.0)
@@ -19,7 +20,6 @@ class FogWeatherView: UIView {
         self.translatesAutoresizingMaskIntoConstraints = false
         
         setupFogImageViews()
-        startFogAnimation()
     }
     
     required init?(coder: NSCoder) {
@@ -27,31 +27,52 @@ class FogWeatherView: UIView {
     }
     
     private func setupFogImageViews() {
-        for i in 0..<fogImageCount {
+        for _ in 0..<fogImageCount {
             let fogImageView = UIImageView(image: UIImage(named: "fog"))
-            fogImageView.contentMode = .scaleAspectFit
+            fogImageView.contentMode = .scaleAspectFill
             fogImageView.translatesAutoresizingMaskIntoConstraints = false
             self.addSubview(fogImageView)
             fogImageViews.append(fogImageView)
-            
-            let offset = CGFloat(i) * 200
-            NSLayoutConstraint.activate([
-                fogImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: offset),
-                fogImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-                fogImageView.heightAnchor.constraint(equalToConstant: 200),
-                fogImageView.widthAnchor.constraint(equalToConstant: 200)  
-            ])
+        }
+        
+        DispatchQueue.main.async {
+            self.startFogAnimation()
         }
     }
     
     private func startFogAnimation() {
-        let animationDuration: TimeInterval = 20.0
+        let animationDuration: TimeInterval = 30.0
         let animationOffset: CGFloat = 200.0
         
-        for fogImageView in fogImageViews {
-            UIView.animate(withDuration: animationDuration, delay: 0, options: [.repeat, .autoreverse, .curveLinear], animations: {
+        guard self.bounds.width >= imageSize else {
+            print("Ошибка: ширина view меньше ширины изображения.")
+            return
+        }
+        
+        for (index, fogImageView) in fogImageViews.enumerated() {
+            let maxXOffset = max(0, self.bounds.width - imageSize)
+            let randomXOffset = CGFloat.random(in: 0...maxXOffset)
+            fogImageView.frame = CGRect(x: randomXOffset, y: self.bounds.height - imageSize, width: imageSize, height: imageSize)
+            
+            let delay = Double(index) * 3.0
+            let duration = animationDuration + Double.random(in: 0...10.0)
+            
+            UIView.animate(withDuration: duration, delay: delay, options: [.repeat, .autoreverse, .curveLinear], animations: {
                 fogImageView.transform = CGAffineTransform(translationX: animationOffset, y: 0)
             }, completion: nil)
         }
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        for fogImageView in fogImageViews {
+            let maxXOffset = max(0, self.bounds.width - imageSize)
+            let randomXOffset = CGFloat.random(in: 0...maxXOffset)
+            fogImageView.frame.origin.x = randomXOffset
+        }
+        
+        startFogAnimation()
+    }
 }
+
